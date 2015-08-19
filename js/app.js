8a1a94047597b001c44c2f39a3556da49854cc57
Nick Harbaugh <njharb@gmail.com>
2015-08-19 12:01:03 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/**
+ * App Title
+ * http://nickharbaugh.com/
+ *
+ * Copyright 2015 Nick Harbaugh | All Rights Reserved
+ * Written by Nick Harbaugh - http://nickharbaugh.com
+ */
+
+var App = (function($, _) {
+
+    // --------------------------------------------- //
+    // initialize
+    // --------------------------------------------- //
+
+    var initialize = function() {};
+
+    // --------------------------------------------- //
+    // return public methods
+    // --------------------------------------------- //
+
+    return {
+        init: initialize
+    };
+
+})(jQuery, _);
